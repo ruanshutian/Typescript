ec13b1bd599c02d567b870834697aa969d40d193
ruanshutian <1204970556@qq.com>
2022-11-10 11:17:15 +0800

    对象接口

---
+++ ts入门/04对象的类型——接口.js
@@ -0,0 +1,19 @@
+/**
+ *
+ */
+//实例化对象的属性数量类型要一致
+var tom = {
+    name: "Tom",
+    age: 25
+};
+var tom2 = {
+    name: "Tome"
+};
+var tom3 = {
+    name: "tom",
+    gender: "male"
+};
+var tom4 = {
+    id: 455,
+    name: "Tom"
+};
--- ts入门/04对象的类型——接口.ts
+++ ts入门/04对象的类型——接口.ts
@@ -28,3 +28,33 @@ let tom2 :Person2 = {
  *
  */
 
+interface Person3{
+    name:string;
+    age?:number;
+    [propName1:string]:any;
+}
+let tom3:Person3 ={
+    name:"tom",
+    gender:"male"
+}
+
+/**
+ * 只读属性
+ */
+interface Person4{
+    readonly id:number,
+    name:string,
+    age?:number;
+    [pro:string]:any;
+}
+let tom4 :Person4 = {
+    id:455,
+    name:"Tom",
+    x:1,
+}
+// tom4.id =1; //只读属性不可以更改
+
+
+
+
+
